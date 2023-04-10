c5f857c54faeaa5c1cfd352c02485388fce134a9
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-04-10 19:35:49 +0200

    setAllDone interaction

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone }) => {
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => {
   if (tasks.length === 0) {
     return null;
   }
@@ -13,6 +13,7 @@ const Buttons = ({ tasks, hideDone, toggleHideDone }) => {
       <button
         className="buttons__button"
         disabled={tasks.every(({ done }) => done)}
+        onClick={setAllDone}
       >
         Ukończ wszystkie
       </button>

d421f4c58b7cff46422a8f956e545818e970601a
Radoslaw Wroblewski <wroblewski082@gmail.com>
2023-04-11 13:31:21 +0200

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import { useState } from "react";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "przejść na Reacta", done: false },
-    { id: 2, content: "zjeść obiad", done: true },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   const toggleHideDone = () => {
     setHideDone((hideDone) => !hideDone);
@@ -42,10 +39,24 @@ function App() {
     );
   };
 
+  const addNewTask = (newTaskContent) => {
+    setTasks((tasks) => [
+      ...tasks,
+      {
+        content: newTaskContent,
+        done: false,
+        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+      },
+    ]);
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie" body={<Form />} />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
       <Section
         title="Lista zadań"
         body={

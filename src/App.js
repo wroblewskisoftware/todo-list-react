80a18bc6bf3ba84f9c2c08efb0bd00f26168e89a
Radoslaw Wroblewski <wroblewski082@gmail.com>
2024-02-11 15:52:50 +0100

    added navigation and subpage about the author

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
 
-const App = () => <Tasks />;
+const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+        <li>
+          <Link to="/autor">O autorze</Link>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      <Route path="/zadania">
+        <Tasks />
+      </Route>
+      <Route path="/autor">
+        <Author />
+      </Route>
+      <Route path="/">
+        <Redirect to="/zadania" />
+      </Route>
+    </Switch>
+  </HashRouter>
+);
 
 export default App;
---
+++ src/features/author/Author.js
@@ -0,0 +1,59 @@
+import { Container } from "../../common/Container/styled";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+const Author = () => (
+  <Container>
+    <Header title="O autorze" />
+    <Section
+      title="Radosław Wróblewski"
+      body={
+        <>
+          <p>
+            Mam na imię Radek i mam 41 lat. Pochodzę z małej miejscowości{" "}
+            <a
+              rel="noreferrer noopener"
+              target="_blanc"
+              href="https://pl.wikipedia.org/wiki/Kutno"
+              title="Otwórz w wikipedii"
+            >
+              Kutno
+            </a>
+            , leżącej w samym centrum Polski. Moja przygoda z programowaniem
+            zaczęła się już w ubiegłym stuleciu, kiedy to rodzice kupili mi
+            komputer{" "}
+            <a
+              rel="noreferrer noopener"
+              target="_blanc"
+              href="https://pl.wikipedia.org/wiki/Commodore_64"
+              title="Otwórz w wikipedii"
+            >
+              <i lang="en">Commodore64</i>
+            </a>
+            . W tym samym okresie dostałem książkę dla dzieci na temat
+            programowania w języku{" "}
+            <a
+              rel="noreferrer noopener"
+              target="_blanc"
+              href="https://pl.wikipedia.org/wiki/BASIC"
+              title="Otwórz w wikipedii"
+            >
+              <i lang="en">BASIC</i>
+            </a>
+            . I tak to się zaczęło. Później zainteresowanie programowaniem
+            zostało zapomniane, wraz z zamianą komputera na rolki.
+            <br />
+            Aż do teraz.
+            <br />
+            Kiedy zobaczyłem zaproszenie na live'a dotyczącego programowania
+            pomyślałem sobie, że może warto spróbować. I w ten sposób zaczął się
+            drugi etap mojej przygody. Mam nadzieję, że niedługo będę mógł
+            wykorzystać nowo zdobyte umiejętności na rynku pracy.
+          </p>
+        </>
+      }
+    />
+  </Container>
+);
+
+export default Author;
